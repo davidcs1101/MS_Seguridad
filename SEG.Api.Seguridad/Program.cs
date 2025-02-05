110b581512662c26607e663816c6a8dc7dfb9f85
davidcs1101 <edisongim@hotmail.com>
2025-02-04 23:21:25 -0500

    PBIXXXXX _ Adicion de CORS para habilitar programa front

--- SEG.Api.Seguridad/Program.cs
+++ SEG.Api.Seguridad/Program.cs
@@ -134,6 +134,18 @@ builder.Services.AddHttpContextAccessor();
 //Servicio que obtiene el UsuarioId del Token
 builder.Services.AddScoped<IUsuarioContextoServicio, UsuarioContextoServicio>();
 
+//OJO:ESTO ES PARA PERMITIR ACCESO DESDE MI FRONT REACT
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Allowhost", builder =>
+        builder
+        //.WithOrigins("https://localhost:3000","","") SOLO MI FRONT
+        .AllowAnyMethod() //OJO: ESTA LINEA NO ES UNA BUENA PRACTICA YA QUE LO IDEAL ES TENER UNA RESTRICCION DE LOS ORIGENES QUE PUEDEN CONSUMIR EL SITIO
+        .AllowAnyHeader()
+        .AllowAnyOrigin());
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -144,7 +156,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors("Allowhost"); // Aquí aplicamos la política CORS para permitir a nuestro FRONT REACT LLEGAR
 app.UseMiddleware<MiddlewareExcepcionesGlobales>();
 
 app.UseAuthentication();
